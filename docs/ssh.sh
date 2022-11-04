03b0c166a74d8006633f501dad473e8ee0b41c34
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-11-04 14:07:30 +0100

    Create ssh.sh

---
+++ docs/ssh.sh
@@ -0,0 +1,10 @@
+# SSH
+
+## Agents
+
+### `gcr-ssh-agent`
+
+`gcr-ssh-agent` is part of/comes with [GNOME gcr](https://gitlab.gnome.org/GNOME/gcr), and
+may be started by a [gcr-ssh-agent.service](https://sandboxdb.org/service/gcr-ssh-agent.service.html),
+as [documented e.g. here](https://wiki.archlinux.org/title/GNOME/Keyring#Enable_the_keyring_ssh_component) and
+was [introduced here](https://gitlab.gnome.org/GNOME/gcr/-/merge_requests/67).
